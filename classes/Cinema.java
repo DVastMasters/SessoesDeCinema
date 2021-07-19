914091381d80d8a06cfb813de23a7ff823364a6e
RafaelD <rafadutr4@outlook.com>
2021-07-19 09:57:04 -0300

    Deleting old files.

--- classes/Cinema.java
+++
@@ -1,8 +0,0 @@
-import java.util.ArrayList;
-
-public class Cinema {
-	private ArrayList<Filme> filmes;
-	private ArrayList<Sala> salas;
-	private ArrayList<Sessao> sessoes;
-	private double faturamento;
-}
