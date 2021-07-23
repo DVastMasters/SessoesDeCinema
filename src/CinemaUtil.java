ca33f0f310a6963ac12c4f2dc78591bb86ab0660
RafaelD <rafadutr4@outlook.com>
2021-07-22 23:39:48 -0300

    Fixed indentation.

--- src/CinemaUtil.java
+++ src/CinemaUtil.java
@@ -10,12 +10,12 @@ public class CinemaUtil{
 		String confirmacao = "";
 
         do {
-			limparTela();
-			if(aberto) {
+		limparTela();
+		if(aberto) {
 
 	    		System.out.println("--------------------------------------------");
-    	    	System.out.println("| -> A venda de ingressos foi iniciada! <- |");
-    	    	System.out.println("|                                          |");
+   			System.out.println("| -> A venda de ingressos foi iniciada! <- |");
+    	    		System.out.println("|                                          |");
            		System.out.println("| 1 - Vender ingresso.                     |");
        			System.out.println("| 2 - Visualizar sessões.                  |");
        			System.out.println("| 3 - Visualizar faturamento.              |");
@@ -23,51 +23,51 @@ public class CinemaUtil{
        			System.out.println("| 5 - Encerrar.                            |");
        			System.out.println("--------------------------------------------");
 
-				System.out.print("\nDigite o número da opção desejada: ");
-			    try {
-					opcao = scanner.nextInt();
-				} catch(java.util.InputMismatchException e) {
-					scanner.next();
-				}
+			System.out.print("\nDigite o número da opção desejada: ");
+			try {
+				opcao = scanner.nextInt();
+			} catch(java.util.InputMismatchException e) {
+				scanner.next();
+			}
 
-				switch(opcao) {
-					case 1:
-						venderIngresso();
-						break;
-					case 2:
-						lerSessoes();
-						break;
-					case 3:
-						lerFaturamento();
-						break;
-					case 4:
-						System.out.println("Ao finalizar a venda de ingressos, todas as sessões serão apagadas e o faturamento será reiniciado.");
-						System.out.print("\nTem certeza que deseja continuar? [S/N]");	
+			switch(opcao) {
+				case 1:
+					venderIngresso();
+					break;
+				case 2:
+					lerSessoes();
+					break;
+				case 3:
+					lerFaturamento();
+					break;
+				case 4:
+					System.out.println("Ao finalizar a venda de ingressos, todas as sessões serão apagadas e o faturamento será reiniciado.");
+					System.out.print("\nTem certeza que deseja continuar? [S/N]");
 						
-						try {
-							confirmacao = scanner.next();
-						} catch(java.util.InputMismatchException e) {}
-						
-						if(confirmacao.equals("s") || confirmacao.equals("S")) {
-							aberto = false;
-							cinema.resetar();
-						}
+					try {
+						confirmacao = scanner.next();
+					} catch(java.util.InputMismatchException e) {}
+					
+					if(confirmacao.equals("s") || confirmacao.equals("S")) {
+						aberto = false;
+						cinema.resetar();
+					}
 				}	
 
 			} else {
 
-		    	System.out.println("------------------------------------------------------");
-    	    	System.out.println("| -> A venda de ingressos ainda não foi iniciada! <- |");
-    	    	System.out.println("|                                                    |");
-       	    	System.out.println("| 1 - Gerenciar sessões.                             |");
-        		System.out.println("| 2 - Gerenciar salas.                               |");
-        		System.out.println("| 3 - Gerenciar filmes.                              |");
-        		System.out.println("| 4 - Inicar venda de ingressos.                     |");
-        		System.out.println("| 5 - Encerrar.                                      |");
-        		System.out.println("------------------------------------------------------");
+		    		System.out.println("------------------------------------------------------");
+    	    			System.out.println("| -> A venda de ingressos ainda não foi iniciada! <- |");
+    	    			System.out.println("|                                                    |");
+       	    			System.out.println("| 1 - Gerenciar sessões.                             |");
+        			System.out.println("| 2 - Gerenciar salas.                               |");
+        			System.out.println("| 3 - Gerenciar filmes.                              |");
+        			System.out.println("| 4 - Inicar venda de ingressos.                     |");
+        			System.out.println("| 5 - Encerrar.                                      |");
+        			System.out.println("------------------------------------------------------");
 				
 				System.out.print("\nDigite o número da opção desejada: ");
-			    try {
+			   	try {
 					opcao = scanner.nextInt();
 				}
 				catch(java.util.InputMismatchException e) {
@@ -75,26 +75,26 @@ public class CinemaUtil{
 				}
 
 				switch(opcao) {
-						case 1:
-							lerSessoes();
-							break;
-						case 2:
-							lerSalas();
-							break;
-						case 3:
-							lerFilmes();
-							break;
-						case 4:
-							if(cinema.getSessoes().length != 0) {
-								System.out.println("É necessário criar uma sessão antes de iniciar as vendas");
-								pausar();	
-							} else {
-								System.out.println("\nApós iniciar as vendas não será possível modificar e nem criar sessões.");
-								System.out.println("\nTem certeza que deseja continuar? [S/N]");
-								try {
-									confirmacao = scanner.next();
-									System.out.println(confirmacao);
-								} catch (java.util.InputMismatchException e) {}
+					case 1:
+						lerSessoes();
+						break;
+					case 2:
+						lerSalas();
+						break;
+					case 3:
+						lerFilmes();
+						break;
+					case 4:
+						if(cinema.getSessoes().length != 0) {
+							System.out.println("É necessário criar uma sessão antes de iniciar as vendas");
+							pausar();	
+						} else {
+							System.out.println("\nApós iniciar as vendas não será possível modificar e nem criar sessões.");
+							System.out.println("\nTem certeza que deseja continuar? [S/N]");
+							try {
+								confirmacao = scanner.next();
+								System.out.println(confirmacao);
+							} catch (java.util.InputMismatchException e) {}
 
 								if(confirmacao.equals("s") || confirmacao.equals("S")) {
 									aberto = true;
